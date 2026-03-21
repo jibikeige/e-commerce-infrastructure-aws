a0c735be38406a1470a6f4219595f93b5535b801
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 00:54:22 -0700

    feat: create external secret operator to fix dependency issues
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- modules/terraform-aws-eks/output.tf
+++ modules/terraform-aws-eks/output.tf
@@ -1,11 +1,11 @@
-output "eks_cluster_name" {
+output "cluster_name" {
   value = aws_eks_cluster.this.name
 }
 
-output "eks_cluster_endpoint" {
+output "cluster_endpoint" {
   value = aws_eks_cluster.this.endpoint
 }
 
-output "eks_cluster_certificate_authority" {
+output "cluster_ca" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
