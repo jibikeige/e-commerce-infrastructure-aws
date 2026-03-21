b9963846ad03eaa456f0c8379a5ea59fcdce9d3b
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 09:45:34 -0700

    feat: install metric server
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- modules/terraform-aws-eks/main.tf
+++ modules/terraform-aws-eks/main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "this" {
   vpc_config {
     subnet_ids              = concat(var.public_subnets, var.private_subnets)
     endpoint_private_access = true
-    endpoint_public_access  = false
+    endpoint_public_access  = true
     public_access_cidrs     = ["0.0.0.0/0"]
   }
 
