4b763621bb44f1d90cae8c7319ebc6fa65602808
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 09:16:29 -0700

    feat: create helm providers
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks/providers.tf
+++ eks/providers.tf
@@ -3,19 +3,31 @@ provider "aws" {
 }
 
 provider "helm" {
+  alias = "exec"
+
   kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = [
+      args = [
         "eks",
         "get-token",
         "--cluster-name",
         module.eks.cluster_name
       ]
     }
+  }
+}
+
+provider "helm" {
+  alias = "token"
+
+  kubernetes = {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
