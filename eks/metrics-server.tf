0e3159324da4c34425b750efb3e37f1bcc32c1b7
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 11:52:46 -0700

    feat: trying to login to the eks cluster
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks/metrics-server.tf
+++ eks/metrics-server.tf
@@ -1,13 +1,13 @@
-#resource "helm_release" "metrics_server" {
-#  name       = "metrics-server"
-#  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-#  chart      = "metrics-server"
-#  namespace  = "kube-system"
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
 
-#  set {
-#    name  = "args"
-#    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
-#  }
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
+  }
 
-#  depends_on = [module.eks]
-#}
+  depends_on = [module.eks]
+}
