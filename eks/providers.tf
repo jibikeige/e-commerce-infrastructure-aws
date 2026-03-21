669b6eef87af9ec5a4c6858abbfefb96dc9bec21
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 09:36:51 -0700

    feat: update the provider
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks/providers.tf
+++ eks/providers.tf
@@ -16,7 +16,7 @@ provider "helm" {
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -26,7 +26,7 @@ provider "kubernetes" {
 
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
