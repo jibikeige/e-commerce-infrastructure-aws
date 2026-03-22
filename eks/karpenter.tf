55663970d7581580f95ab656888a2e6f8af658e5
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 17:21:45 -0700

    feat: use proper syntax for node pool
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -177,22 +177,34 @@ YAML
 
 resource "kubectl_manifest" "karpenter_node_pool" {
   yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1
-    kind: NodePool
-    metadata:
-      name: default
+apiVersion: karpenter.sh/v1
+kind: NodePool
+metadata:
+  name: default
+spec:
+  template:
     spec:
-      template:
-        spec:
-          nodeClassRef:
-            name: default
-          requirements:
-            - key: kubernetes.io/arch
-              operator: In
-              values: ["amd64"]
-      limits:
-        cpu: "1000"
-  YAML
+      nodeClassRef:
+        group: karpenter.k8s.aws
+        kind: EC2NodeClass
+        name: default
+
+      requirements:
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64"]
+
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand", "spot"]
+
+        - key: node.kubernetes.io/instance-type
+          operator: In
+          values: ["t3.small", "t3.medium", "t3.large"]
+
+  limits:
+    cpu: "1000"
+YAML
 
   depends_on = [kubectl_manifest.karpenter_node_class]
 }
