437f56adf347fcbd8e3b12bd10dce4a829f1f185
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 18:15:31 -0700

    feat: remove aws auth
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- modules/terraform-aws-eks/main.tf
+++ modules/terraform-aws-eks/main.tf
@@ -65,25 +65,3 @@ resource "aws_eks_access_policy_association" "this" {
 
   depends_on = [aws_eks_access_entry.this]
 }
-
-resource "kubernetes_config_map_v1_data" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_node_role.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = ["system:bootstrappers", "system:nodes"]
-      }
-    ])
-    mapUsers = yamlencode(var.aws_auth_users)
-  }
-
-  force = true
-
-  depends_on = [aws_eks_node_group.this]
-}
