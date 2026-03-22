b334f4f6821cf0d2666071a6431e217d2e348c2a
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-21 17:26:04 -0700

    feat: create metric servers
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks/metrics-server.tf
+++ eks/metrics-server.tf
@@ -1,13 +1,13 @@
-#resource "helm_release" "metrics_server" {
-#  name       = "metrics-server"
-#  repository = "https://kubernetes-sigs.github.io/metrics-server/"
-#  chart      = "metrics-server"
-#  namespace  = "kube-system"
-#  version    = "v0.8.1"
-#  set {
-#    name  = "args"
-#    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
-#  }
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "v0.8.1"
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP}"
+  }
 
-#  depends_on = [module.eks]
-#}
+  depends_on = [module.eks]
+}
