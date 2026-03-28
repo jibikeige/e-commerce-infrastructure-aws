c9c323d84fdc4d515bae9ecaca151da3e2ee42d8
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-28 07:39:41 -0700

    feat: put secrets in secret manager
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- modules/terraform-aws-rds/outputs.tf
+++ modules/terraform-aws-rds/outputs.tf
@@ -1,15 +1,23 @@
-output "db_endpoint" {
-  value = aws_db_instance.this.endpoint
+output "db_host" {
+  value = aws_db_instance.this.address
 }
 
 output "db_port" {
-  value = aws_db_instance.this.port
+  value = tostring(aws_db_instance.this.port)
 }
 
-output "db_security_group_id" {
-  value = aws_security_group.rds.id
+output "db_name" {
+  value = aws_db_instance.this.db_name
 }
 
-output "db_identifier" {
-  value = aws_db_instance.this.id
+output "db_username" {
+  value = aws_db_instance.this.username
+}
+
+output "db_secret_arn" {
+  value = aws_secretsmanager_secret.db.arn
+}
+
+output "db_secret_name" {
+  value = aws_secretsmanager_secret.db.name
 }
--- modules/terraform-aws-rds/secrets.tf
+++ modules/terraform-aws-rds/secrets.tf
@@ -9,7 +9,7 @@ resource "random_id" "suffix" {
 }
 
 resource "aws_secretsmanager_secret" "db" {
-  name = "${var.name}-postgres-credentials-${random_id.suffix.hex}"
+  name = "${var.name}-postgres-credentials-${random_id.suffix.hex}/db-credentials"
 
   recovery_window_in_days = 0
 
@@ -23,6 +23,11 @@ resource "aws_secretsmanager_secret_version" "db" {
   secret_id = aws_secretsmanager_secret.db.id
 
   secret_string = jsonencode({
+    host     = aws_db_instance.this.address
+    port     = tostring(aws_db_instance.this.port)
+    db       = aws_db_instance.this.db_name
+    username = aws_db_instance.this.username
     password = random_password.db.result
+    url      = "postgresql://${aws_db_instance.this.username}:${random_password.db.result}@${aws_db_instance.this.address}:${aws_db_instance.this.port}/${aws_db_instance.this.db_name}"
   })
 }
