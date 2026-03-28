6eae3286e35a9f0f17f0062d2a4ffb9c1a8d9c1b
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-28 11:50:49 -0700

    feat: fix external dns issues
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- modules/terraform-aws-eks-dependency/locals.tf
+++ modules/terraform-aws-eks-dependency/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  oidc_provider_url = replace(var.aws_eks_cluster_identity, "https://", "")
-  oidc_provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider_url}"
+  oidc_provider     = replace(var.aws_eks_cluster_identity, "https://", "")
+  oidc_provider_url = local.oidc_provider
+  oidc_provider_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider}"
 }
