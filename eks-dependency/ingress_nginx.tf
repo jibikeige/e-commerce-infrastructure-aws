a1985030871a5ce047008bf4c465e9eedc553a9d
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-27 22:00:48 -0700

    feat: create nginx ingress using terraform
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- eks-dependency/ingress_nginx.tf
+++
@@ -1,6 +0,0 @@
-resource "kubernetes_namespace" "ingress_nginx" {
-  metadata {
-    name = "ingress-nginx"
-  }
-}
-
---
+++ modules/terraform-aws-eks-dependency/ingress_nginx.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace" "ingress_nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  # Equivalent to -f ./nginx-ingress.yaml
+  values = [
+    file("${path.module}/nginx-ingress/nginx-ingress.yaml")
+  ]
+
+  depends_on = [kubernetes_namespace.ingress_nginx]
+}
