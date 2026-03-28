89d5461c1186b3b08d43151423b28f7aaa233afd
Ajibike Ige <jibike.majaro@gmail.com>
2026-03-28 06:29:59 -0700

    feat: fixed base path
    Signed-off-by: Ajibike Ige <jibike.majaro@gmail.com>

--- application/main.tf
+++ application/main.tf
@@ -3,9 +3,9 @@ locals {
 
   manifests = {
     for svc in local.services : svc => {
-      deployment = yamldecode(file("${path.root}/services/${svc}/deployment.yaml"))
-      service    = yamldecode(file("${path.root}/services/${svc}/service.yaml"))
-      hpa        = yamldecode(file("${path.root}/services/${svc}/hpa.yaml"))
+      deployment = yamldecode(file("${path.module}/services/${svc}/deployment.yaml"))
+      service    = yamldecode(file("${path.module}/services/${svc}/service.yaml"))
+      hpa        = yamldecode(file("${path.module}/services/${svc}/hpa.yaml"))
     }
   }
 }
@@ -18,6 +18,5 @@ module "applications" {
   aws_eks_cluster_identity = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
   cluster_name             = data.aws_eks_cluster.this.name
   cluster_endpoint         = data.aws_eks_cluster.this.endpoint
-
-  manifests = local.manifests
+  manifests                = local.manifests
 }
